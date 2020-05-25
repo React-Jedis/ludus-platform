6b819895cb13886ec5c05453cbd94d7a86d9e91f
Rubén <rubenofen@gmail.com>
2020-05-25 02:03:50 +0200

    content demo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,10 +10,10 @@ const Header = ({ title }) => {
     <div className="flex items-center justify-between bg-corporative-3 min-h-35 pl-2 pr-2">
       <Burger
         width="2.5rem"
-        className="pb-1 pt-1 pl-2 pr-2 m-2 border rounded-md"
+        className="pb-1 pt-1 pl-2 pr-2 m-2 border rounded-md hover:bg-black hover:bg-opacity-25 hover:opacity-75 cursor-pointer"
         onClick={() => setOpenDraw(true)}
       />
-      <span>{title}</span>
+      <span className="text-xl">{title}</span>
       <img src={logo} alt="Logo" width="40px" />
     </div>
   );
